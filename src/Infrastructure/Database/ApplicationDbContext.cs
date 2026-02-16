b89a365038b203e8353c13d80b7ffcb81456b287
Dmitrii Matov <matovxd@gmail.com>
2026-02-15 20:07:58 -0700

    initial commit

---
+++ src/Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Application.Abstractions.Data;
+using Domain.Users;
+using Infrastructure.DomainEvents;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Infrastructure.Database;
+
+public sealed class ApplicationDbContext(
+    DbContextOptions<ApplicationDbContext> options,
+    IDomainEventsDispatcher domainEventsDispatcher)
+    : DbContext(options), IApplicationDbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        modelBuilder.HasDefaultSchema(Schemas.Default);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        int result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+
+        return result;
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                List<IDomainEvent> domainEvents = entity.DomainEvents;
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        await domainEventsDispatcher.DispatchAsync(domainEvents);
+    }
+}
